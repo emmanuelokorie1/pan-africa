f473feb4da7fb93bc5e066c203b00f52b063eeeb
Okorie Emmanuel <okorieemmanuel167@gmail.com>
2024-08-15 09:37:47 +0100

    update: changed the favicon and name of the projec

--- src/Pages/EGS/EGS.tsx
+++ src/Pages/EGS/EGS.tsx
@@ -1,18 +1,25 @@
-import HeaderNav from "../Reuseable/UI/HeaderNav"
+import ContactTemp from "../Reuseable/UI/ContactTemp";
+import HeaderNav from "../Reuseable/UI/HeaderNav";
 
 function EGS() {
   return (
     <div>
-      <HeaderNav
-        title={"ESG"}
-        headerText={"Environmental, Social and Goverance"}
-        text={
-          "We are committed to the integration of environmental and social considerations into all aspects of our business activities for the promotion of a sustainable company."
-        }
-        noImage={true}
-      />
+      <div>
+        <HeaderNav
+          title={"ESG"}
+          headerText={"Environmental, Social and Goverance"}
+          text={
+            "We are committed to the integration of environmental and social considerations into all aspects of our business activities for the promotion of a sustainable company."
+          }
+          noImage={true}
+        />
+      </div>
+
+      <div className="py-[2rem]">
+        <ContactTemp />
+      </div>
     </div>
-  )
+  );
 }
 
-export default EGS
+export default EGS;
